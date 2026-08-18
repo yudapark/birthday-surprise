0317ead5c1df9e67685cfed1c1d4a18429ce2806
yuda <yuda9674@gmail.com>
2026-08-18 20:43:10 +0700

    Reorder sections in navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -6,11 +6,11 @@ const sections = [
     'section-opening', 
     'section-question', 
     'section-story', 
-    'section-gallery',
-    'section-video', 
+    'section-gallery', 
     'section-quiz', 
     'section-lovecalc',
     'section-unlock',
+    'section-video',
     'section-letter', 
     'section-final'
 ];
